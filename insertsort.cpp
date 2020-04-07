4f2630f4ef36fcdc59172a8050e3912f06816ff4
katiejyoung <kjy3@georgetown.edu>
2020-04-07 10:37:46 -0700

    Create C++ template files and implement compiler

---
+++ insertsort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void lineToArray();
+
+int main() {
+    cout << "Hello, world." << endl;
+    return 0;
+}
+
+void lineToArray() {
+
+}
