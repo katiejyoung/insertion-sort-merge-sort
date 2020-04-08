022532022578f9be7f2f9d6ebd28cbd28e9a65bc
katiejyoung <kjy3@georgetown.edu>
2020-04-08 09:34:54 -0700

    Add comments to insertsort

--- insertsort.cpp
+++ insertsort.cpp
@@ -15,10 +15,11 @@ void saveValueToIndex(vector<int> &vect, int length, int value, int newIndex, in
 void writeToFile(vector<int> &vect, int length);
 
 int main() {
-    int removeFile = remove("insert.out");
+    int removeFile = remove("insert.out"); // Delete output file in case program has already been run
     string list = "data.txt";
-    int success = readLines(list);
+    int success = readLines(list); // Initiate program
 
+    // Output message according to program success
     if (success == 1) {
         cout << "Insertion sort complete." << endl;
     }
@@ -29,45 +30,55 @@ int main() {
     return 0;
 }
 
+// Reads file line by line and initiates sorting
+// Returns 1 if sorting successful
+// Returns 0 if file error
 int readLines(string dataFile) {
     string fileLine;
-    ifstream inputFile(dataFile.c_str());
     int len;
     vector<int> valueVector;
 
+    ifstream inputFile(dataFile.c_str()); // Open passed file
 
+    // If open successful, read lines
     if (inputFile.is_open()) {
-        while(getline(inputFile, fileLine)) {
+        while(getline(inputFile, fileLine)) { // Iterate through file lines
+            // Obtain new vector size and update vector length accordingly
             len = getVectorSize(fileLine);
             valueVector.resize(len);
 
+            // Convert line to vector
             lineToArray(valueVector, fileLine, len);
 
+            // Sort vector
             insertSort(valueVector, len);
-            valueVector.clear();
+            valueVector.clear(); // Clear vector elements
         }
 
-        inputFile.close();
-        return 1;
+        inputFile.close(); // Close ifstream
+        return 1; // Return 1 if successful
     }
-
     else {
-        return 0;
+        return 0; // Return 0 if unsuccessful
     }  
 }
 
+// Identifies first integer in a given string
+// Returns integer as vector size
 int getVectorSize(string dataString) {
     int dataItr = 0;
     int dataValue = 0;
     string dataItem;
 
+    // Iterate through string
     while (dataItr <= dataString.length()) {
+        // When separator identified, convert to integer and return
         if ((dataString[dataItr] == ' ') || (dataItr == dataString.length())) {
             dataValue = atoi(dataItem.c_str());
             return dataValue;
         }
         else {
-            dataItem += dataString[dataItr];
+            dataItem += dataString[dataItr]; // Append character to string until separator reached
         }
 
         dataItr++;
@@ -76,6 +87,7 @@ int getVectorSize(string dataString) {
     return 0;
 }
 
+// Converts a passd string to a vector by separating integers from spaces
 void lineToArray(vector<int> &vect, string dataString, int length) {
     int dataItr = 0;
     int vectorItr = 0;
@@ -83,70 +95,81 @@ void lineToArray(vector<int> &vect, string dataString, int length) {
     int dataValue = 0;
     string dataItem;
 
+    // Iterate through string
     while ((dataItr <= dataString.length()) && (vectorItr < length)) {
-        if ((dataString[dataItr] == ' ') || (dataItr == dataString.length())) {
-            dataValue = atoi(dataItem.c_str());
+        // When separator identified, convert value to integer and append to vector
+        if ((dataString[dataItr] == ' ') || (dataItr == dataString.length())) { 
+            dataValue = atoi(dataItem.c_str()); // Convert value to integer
             
+            // Append value to vector
             if (isLength != 1) {
                 vect[vectorItr] = dataValue;
                 vectorItr++;
             }
             else {
-                isLength = 0;
+                isLength = 0; // Ignore length marker
             }
             
             dataValue = 0;
             dataItem = "";
         }
         else {
-            dataItem += dataString[dataItr];
+            dataItem += dataString[dataItr]; // Append character to string until separator reached
         }
 
         dataItr++;
     }
-
 }
 
+// Iterates through array and identifies values to be shifted
+// Passes to saveValueToIndex to change element index
 void insertSort(vector<int> &vect, int length) {
     int largeValIdx = 0;
     int largeVal = vect[largeValIdx];
     int newIndex;
     int i, k;
     
+    // Iterate through array
     for (i = 0; i < length; i++) {
+        // Identify values to be shifted
         if (vect[i] < largeVal) {
             k = 0;
-            while (vect[k] <= vect[i]) {
+            while (vect[k] <= vect[i]) { // Identify index to which the value should be moved
                 k++;
             }
 
-            saveValueToIndex(vect, length, vect[i], k, i);
+            saveValueToIndex(vect, length, vect[i], k, i); // Shift array to right and move value to identified index
         }
         else {
-            largeVal = vect[i];
+            largeVal = vect[i]; // Update largest value tracker if in correct order
         }
         largeValIdx++;
     }
 
-    writeToFile(vect, length);
+    writeToFile(vect, length); // Output results when finished
 }
 
+// Moves passed value to specified index and
+// Shifts existing elements to right
 void saveValueToIndex(vector<int> &vect, int length, int value, int newIndex, int oldIndex) {
     vector<int> tempVector;
     tempVector.resize(length);
     int i = 0;
     int j = 0;
 
+    // Copy elements to left of new index to temporary vector
     while (i < newIndex) {
         tempVector[i] = vect[i];
         i++;
     }
 
+    // Copy new value to passed index
     tempVector[i] = value;
     j = i + 1;
 
+    // Append remaining elements in old vector to temporary vector
     while (i < length) {
-        if (i != oldIndex) {
+        if (i != oldIndex) { // Skip the shifted value
             tempVector[j] = vect[i];
             j++;
         }
@@ -154,6 +177,7 @@ void saveValueToIndex(vector<int> &vect, int length, int value, int newIndex, in
         i++;
     }
 
+    // Replace passed vector with new array element order
     for (i = 0; i < length; i++) {
         vect[i] = tempVector[i];
     }
@@ -161,13 +185,13 @@ void saveValueToIndex(vector<int> &vect, int length, int value, int newIndex, in
 
 // Writes passed vector elements to "insert.out"
 void writeToFile(vector<int> &vect, int length) {
-    ofstream outputFile("insert.out", ios_base::app);
+    ofstream outputFile("insert.out", ios_base::app); // Open ofstream and enable overwriting
 
+    // Write elements to file
     for (int i = 0; i < length; i++) {
         outputFile << vect[i] << " ";
     }
-
     outputFile << endl;
 
-    outputFile.close();
+    outputFile.close(); // Close ofstream
 }
