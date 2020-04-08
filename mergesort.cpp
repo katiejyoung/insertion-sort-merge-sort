94b4cc506711abd52109902c7007c03a9286f266
katiejyoung <kjy3@georgetown.edu>
2020-04-08 09:06:11 -0700

    Implement mergesort

--- mergesort.cpp
+++ mergesort.cpp
@@ -10,7 +10,9 @@ using namespace std;
 int readLines(string dataFile);
 int getVectorSize(string dataString);
 void lineToArray(vector<int> &vect, string dataString, int length);
-void MergeSort(vector<int> &vect, int length);
+void sortElements(vector<int> &vect, int length);
+void mergeSort(vector<int> &vect, int left, int right);
+void merge(vector<int> &vect, int left, int right, int middle);
 void writeToFile(vector<int> &vect, int length);
 
 int main() {
@@ -42,7 +44,7 @@ int readLines(string dataFile) {
 
             lineToArray(valueVector, fileLine, len);
 
-            MergeSort(valueVector, len);
+            sortElements(valueVector, len);
             valueVector.clear();
         }
 
@@ -107,10 +109,88 @@ void lineToArray(vector<int> &vect, string dataString, int length) {
 
 }
 
-void MergeSort(vector<int> &vect, int length) {
-    
+void sortElements(vector<int> &vect, int length) {
+    mergeSort(vect, 0, (length - 1));
 
     writeToFile(vect, length);
+}
+
+void mergeSort(vector<int> &vect, int left, int right) {
+    if (left < right) {
+        int middle = (left + right)/2;
+        // cout << "Left index: " << left << endl;
+        // cout << "Middle index: " << middle << endl;
+        // cout << "Right index: " << right << endl;
+
+        // cout << "Sorting left-middle index: " << left << ", " << middle << endl;
+        mergeSort(vect, left, middle);
+        // cout << "Sorting middle+1-right index: " << (middle + 1) << ", " << right << endl;
+        mergeSort(vect, (middle + 1), right);
+
+        merge(vect, left, right, middle);
+
+        // cout << "Merged vector: ";
+        // for (int i = 0; i <= right; i++) {
+        //     cout << vect[i] << " ";
+        // }
+        // cout << endl;
+    }
+}
+
+void merge(vector<int> &vect, int left, int right, int middle) {
+    vector<int> vectorLeft;
+    vector<int> vectorRight;
+    int vectorLeftSize = middle - left + 1;
+    int vectorRightSize = right - middle;
+    int i;
+    int j = 0;
+    int k = left;
+
+    vectorLeft.resize(vectorLeftSize);
+    vectorRight.resize(vectorRightSize);
+
+    // cout << "Left Vector: ";
+    for (i = 0; i < vectorLeftSize; i++) {
+        vectorLeft[i] = vect[left + i];
+        // cout << vectorLeft[i] << " ";
+    }
+    // cout << endl << "Right Vector: ";
+    for (i = 0; i < vectorRightSize; i++) {
+        vectorRight[i] = vect[middle + i + 1];
+        // cout << vectorRight[i] << " ";
+    }
+    // cout << endl;
+
+    i = 0;
+    while ((i < vectorLeftSize) && (j < vectorRightSize)) {
+        if (vectorLeft[i] <= vectorRight[j]) {
+            vect[k] = vectorLeft[i];
+            i++;
+        }
+        else {
+            vect[k] = vectorRight[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < vectorLeftSize) {
+        vect[k] = vectorLeft[i];
+        i++;
+        k++;
+    }
+
+    while (j < vectorRightSize) {
+        vect[k] = vectorRight[j];
+        j++;
+        k++;
+    }
+
+    // cout << "Updated Vector: ";
+    // for (i = 0; i < 4; i++) {
+    //     cout << vect[i] << " ";
+    // }
+    // cout << endl;
 }
 
 void writeToFile(vector<int> &vect, int length) {
